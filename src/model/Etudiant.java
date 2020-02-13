ffe0af075401cf0bab15bf97f1f87c720179b5d3
zeushias <zeushias89@gmail.com>
2020-02-13 11:44:47 +0100

    classe étudiant

---
+++ src/model/Etudiant.java
@@ -0,0 +1,98 @@
+package model;
+
+import java.sql.Date;
+
+public class Etudiant {
+
+	private int id;
+	private String nom;
+	private String prenom;
+	private String email;
+	private String adresse;
+	private String telephone;
+	private Date dateNaiss;
+
+	public Etudiant(int id, String nom, String prenom, String email, String adresse, String telephone, Date dateNaiss) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.adresse = adresse;
+		this.telephone = telephone;
+		this.dateNaiss = dateNaiss;
+	}
+
+	public Etudiant(String nom, String prenom, String email, String adresse, String telephone, Date dateNaiss) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.adresse = adresse;
+		this.telephone = telephone;
+		this.dateNaiss = dateNaiss;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getAdresse() {
+		return adresse;
+	}
+
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public Date getDateNaiss() {
+		return dateNaiss;
+	}
+
+	public void setDateNaiss(Date dateNaiss) {
+		this.dateNaiss = dateNaiss;
+	}
+
+	@Override
+	public String toString() {
+		return "Etudiant [id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", email=" + email + ", adresse="
+				+ adresse + ", telephone=" + telephone + ", dateNaiss=" + dateNaiss + "]";
+	}
+
+}
