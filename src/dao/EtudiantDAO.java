d978b37d8ca43e4bd3a24152e97d29963393de65
Braham Moussouni <braham.moussouni@etudiant.univ-lr.fr>
2020-02-13 12:44:03 +0100

    methode1

--- src/dao/EtudiantDAO.java
+++ src/dao/EtudiantDAO.java
@@ -3,6 +3,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import model.Etudiant;
+
 
 public class EtudiantDAO{
 			private static Statement st = null;
@@ -10,24 +12,23 @@ public class EtudiantDAO{
 			private static String sql = "";
 			private static Bdd B= new Bdd(); 
 
-			public static String get_nom_by_id(int a) throws ClassNotFoundException, SQLException {
-		//connexion bdd
+			public static Etudiant get_nom_by_id(int a) throws ClassNotFoundException, SQLException {
+				//connexion bdd
 				B.connection();
 				//traitement
 				st = B.getCn().createStatement();
-				sql = "Select nom from etudiant Where id ="+Integer.toString(a);
+				sql = "Select * from etudiant Where id ="+Integer.toString(a);
 				rs = st.executeQuery(sql);
 				if(rs.next()){ 
+				return Etudiant(rs.getString(1),rs.getString(2),rs.getString(3),rs.getString(4),rs.getString(5),rs.getString(6),rs.getString(7));
 					
-					System.out.println(rs.getString(1));
-					return (rs.getString(1));
 				}
 				else{
 					System.out.println("Record Not Found...");
 				}
 				//deconnexion bdd
 				B.deconnection();
-				return "Null";
+				return null;
 		
 		
 	}
