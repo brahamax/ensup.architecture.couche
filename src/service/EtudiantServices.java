8749774cab98edf67240e7422d0fd499e0f356da
Braham Moussouni <braham.moussouni@etudiant.univ-lr.fr>
2020-02-13 16:55:25 +0100

    Update EtudiantServices.java

--- src/service/EtudiantServices.java
+++ src/service/EtudiantServices.java
@@ -22,7 +22,7 @@ public class EtudiantServices {
 	/**
 	  * Afficher le resultat de l'insertion de l'etudiant
 	  * @param Etudiant e
-	  * @return Stringg
+	  * @return String
 	  */
 	public String insererEtudiant(Etudiant e) throws ClassNotFoundException, SQLException{
 		
