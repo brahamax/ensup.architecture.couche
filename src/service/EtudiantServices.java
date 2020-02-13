e29c3ddbefc4fe154096759f0f8383aede5c7e97
Braham Moussouni <braham.moussouni@etudiant.univ-lr.fr>
2020-02-13 14:58:28 +0100

    n,b

--- src/service/EtudiantServices.java
+++ src/service/EtudiantServices.java
@@ -5,43 +5,22 @@ import java.sql.SQLException;
 import dao.EtudiantDAO;
 import model.Etudiant;
 
-public class EtudiantServices implements EtudiantService {
-EtudiantDAO etudiantDao;
+public class EtudiantServices {
 
 
-	/**
- * @param etudiantDao
- */
-public EtudiantServices() {
-	 etudiantDao = new EtudiantDAO();;
-}
+	public String afficherEtudiant( int id) throws ClassNotFoundException, SQLException {
 
-	@Override
-	public void methode() {
-		// TODO Auto-generated method stub
+		return EtudiantDAO.getEtudiant(id).toString();
 		
+	public String insererEtudiant (Etudiant e){
+		
+		if (EtudiantDAO.insertEtudiant(e)== 0) {
+			return "Etudiant inseré";
+		}
+		return "Etudiant non inséré";
 	}
 	
-	// methodes metiers 
 	
-	// comparer deux etudiants ( age) 
-	/*
-	 * 
-	 */
-	public  String compareAge(Etudiant etu1, Etudiant etu2) {
-		String result ="";
-		if(etu1.getDateNaiss().before(etu2.getDateNaiss())) 
-			result = etu1.getPrenom() + " " + etu1.getNom() +" est plus agé  ";
-		else
-			result = etu2.getPrenom() + " " + etu2.getNom() +" est plus agé  ";
-		
-		 return result;
-	}
-
-	@Override
-	public Etudiant lireEtudiant( int id) throws ClassNotFoundException, SQLException {
-
-		return etudiantDao.getEtudiant(id);
 	}
 	
 
